51fbe01e866237b0681b5f8beb896eaed3b7bdc2
Jim Kueneman <JimKueneman@yahoo.com>
2023-11-04 17:17:24 -0700

    Another TCP bug fix

--- Target-PC/HardwareConnection/lcc_ethernet_server.pas
+++ Target-PC/HardwareConnection/lcc_ethernet_server.pas
@@ -656,7 +656,7 @@ begin
     if TcpDecodeStateMachine.OPStackcoreTcp_DecodeMachine(StreamReadByte(WorkerStream), LocalDataArray) then
     begin
       if WorkerMessage.LoadByLccTcp(LocalDataArray) then
-        AliasServerThread.AddIncomingLccMessage(WorkerMessage, False);
+        AliasServerThread.AddIncomingLccMessage(WorkerMessage, True);
     end;
   end;
 end;
